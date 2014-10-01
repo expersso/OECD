ed33572a95e86123d977f51db0becf93fba1557c
Eric Persson <ericpersson1@gmail.com>
2014-10-01 16:17:32 +0200

    Split functions into main and helper functions. Finished function documentation.

--- R/oecd.R
+++ R/oecd.R
@@ -1,55 +1,21 @@
-library(XML)
-library(lubridate)
-
-# Make urls for GET requests
-makeURL <- function(query, filter = "") {
-  filter <- paste0(filter, collapse = " and ")
-  filter <- ifelse(filter == "", "", 
-                   paste0("$filter=", filter, "&"))
-  url <- paste0("http://stats.oecd.org/OECDStatWCF_OData/OData.svc/", 
-                query, 
-                filter, 
-                "$format=json")
-  url <- str_replace_all(url, " ", "%20")
-  return(url)
-}
-
-# Make filter string for countries
-makeCountryFilter <- function(country = "all", country_term = "LOCATION") {
-  suppressWarnings(
-    if(country != "all") {
-    countries <- paste0(sprintf("(%s eq '%s')", country_term, country), collapse = " or ")
-    return(countries)
-  } else return(""))
-}
-
-# Make filter string for time
-makeTimeFilter <- function(time = NULL, time_term = "TIME") {
-  suppressWarnings(
-    if(!is.null(time)) {
-      time <- sprintf("(%s ge '%s')", time_term, time)
-      return(time)
-    } else {
-      return("")
-    })
-}
-
 #' Get the dimensions of a dataset.
 #' 
-#' Rreturs a data frame containing names and descriptions 
-#' of the variables of a specified series.
+#' Returs a data frame containing names and descriptions of the variables 
+#' of a specified series.
 #' 
 #' @param series A string containing the code for a series.
 #' 
 #' @return A data frame.
 #' 
-#' @seealso \code{\link{getDatasets}}, \code{\link{searchSeries}}
+#' @seealso \code{\link{getDesc}} for similar functionality regarding variable
+#' values.
 #' 
 #' @examples
 #' getDimensions("DUR_D")
+#' @export
 getDimensions <- function(series) {
   url <- makeURL(query = paste0("GetDimension?DatasetCode=", series, "&"))
-  data <- content(GET(url))
+  data <- httr::content(httr::GET(url))
   data <- do.call(rbind, lapply(data[[2]], data.frame, stringsAsFactors = FALSE))
   return(data)
 }
@@ -68,9 +34,10 @@ getDimensions <- function(series) {
 #' @examples
 #' datasets <- getDatasets()
 #' datasets[1:10, 1:2]
+#' @export
 getDatasets <- function() {
   url <- makeURL("GetDatasets?")
-  data <- content(GET(url))
+  data <- httr::content(httr::GET(url))
   data <- data[[2]]
   data <- ldply(data, "unlist")
   return(data)
@@ -98,8 +65,9 @@ getDatasets <- function() {
 #' @examples
 #' datasets <- getDatasets()
 #' searchSeries(datasets, "unemployment")
+#' @export
 searchSeries <- function(data = getDatasets(), string = "unemployment", metadata = FALSE) {
-  results <- data[str_detect(data$DatasetTitle, ignore.case(string)),]
+  results <- data[stringr::str_detect(data$DatasetTitle, ignore.case(string)),]
   if(!metadata) results <- results[,-3]
   return(results)
 }
@@ -119,6 +87,7 @@ searchSeries <- function(data = getDatasets(), string = "unemployment", metadata
 #' @examples
 #' df <- getSeries("D_DUR", country = c("DEU", "FRA"), since = 2008)
 #' head(df)
+#' @export
 getSeries <- function(series, country = "all", since = NULL) {
   df_dim <- getDimensions(series)
   country_term <- df_dim$DimensionCode[df_dim$DimensionCode %in% c("COU", "COUNTRY", "LOCATION")]
@@ -134,20 +103,12 @@ getSeries <- function(series, country = "all", since = NULL) {
                                  makeTimeFilter(time = since, 
                                                 time_term = time_term)))
   
-  result <- content(GET(url))
-  result <- rbind.fill(lapply(result[[2]], function(f) {
+  result <- httr::content(httr::GET(url))
+  result <- plyr::rbind.fill(lapply(result[[2]], function(f) {
     as.data.frame(Filter(Negate(is.null), f), stringsAsFactors = FALSE)
   }))
   names(result) <- tolower(names(result))
   return(result)
-}
-
-# Get XML with variable descriptions
-getCodelistnode <- function(series) {
-  url <- paste0("http://stats.oecd.org/restsdmx/sdmx.ashx/GetDataStructure/", series)
-  xml_desc <- content(GET(url))
-  codelist <- xmlRoot(xml_desc)[["CodeLists"]]
-  return(codelist)
 }
 
 #' Get a list of data frames with variable descriptions.
@@ -157,26 +118,70 @@ getCodelistnode <- function(series) {
 #' 
 #' The data frames returned with \code{\link{getSeries}} often contain variables
 #' whose values are not obvious. For example, 
-#' \code{\link{getSeries("MIG_UNEMP_GENDER")}}
+#' \code{\link{getSeries("MIG_UNEMP_GENDER")}} returns a data frame with the 
+#' variable \code{birth} which has the values \code{FB} and \code{NB}, and the
+#' variable \code{rate} which has the values \code{N_RATE} and \code{U_RATE}.
+#' Since the meaning of these are not obvious, \code{getDesc} returns a list of 
+#' data frames that can serve as human-readable look-up tables of variables 
+#' (see examples below).
+#' 
+#' @param series A string specifying the code of the series.
+#' 
+#' @return A list of data frames, each corresponding to one variable in the 
+#' specified series.
+#' 
+#' @seealso \code{\link{getDimensions}} provides similar functionality for 
+#' variables names (e.g. getDimensions("MIG_UNEMP_GENDER"), to learn that 
+#' the variable \code{birth} refers to "place of birth".
+#' 
+#' @examples
+#' series <- "MIG_UNEM_GENDER"
+#' df <- getSeries(series)
+#' unique(df$birth)
+#' desc <- getDesc(series)
+#' desc$birth
+#' 
+#' @export
 getDesc <- function(series) {
   codelist_node <- getCodelistnode(series)
-  codelist_children <- xmlChildren(codelist_node)
+  codelist_children <- XML::xmlChildren(codelist_node)
   df_desc_list <- list()
   
   for(i in 2:length(names(codelist_children))) {
-    list <- xmlChildren(codelist_children[[i]])[-1] 
-    df_code_temp <- do.call(rbind, lapply(list, xmlAttrs))
-    df_desc_temp <- xmlToDataFrame(list)
-    df_desc <- data.frame(df_code_temp, df_desc_temp, row.names = NULL, stringsAsFactors = FALSE)
+    list <- XML::xmlChildren(codelist_children[[i]])[-1] 
+    df_code_temp <- do.call(rbind, lapply(list, XML::xmlAttrs))
+    df_desc_temp <- XML::xmlToDataFrame(list)
+    df_desc <- data.frame(df_code_temp, df_desc_temp, 
+                          row.names = NULL, stringsAsFactors = FALSE)
     df_desc <- df_desc[,-ncol(df_desc)]
     names(df_desc) <- tolower(names(df_desc))
     df_desc_list[[i-1]] <- df_desc
   }
-  names(df_desc_list) <- tolower(str_extract(sapply(codelist_children, xmlGetAttr, "id")[-1], "[A-Z]*$"))
+  names(df_desc_list) <- tolower(stringr::str_extract(sapply(codelist_children, 
+                                                               XML::xmlGetAttr, 
+                                                               "id")[-1], 
+                                                        "[A-Z]*$"))
   return(df_desc_list)
 }
 
-# Browse the metadata related to a series
+#' Browse the metadata related to a series.
+#' 
+#' Opens up a web browser with the metadata related to the requested series.
+#' 
+#' @param series A string specifying the code of the series.
+#' @param data A data frame with the data set codes, titles, and metadata. 
+#' This can be either a data frame previously fetched using 
+#' \code{\link{getDatasets}} (recommended) or left blank, in which case a 
+#' temporary data frame is fetched. The second option adds a few seconds to 
+#' each browse request.
+#' 
+#' @return Opens a temporary XML file in the default web browser.
+#' 
+#' @examples
+#' datasets <- getDatasets()
+#' getMetadata(datasets, "DUR_D")
+#' 
+#' @export
 browseMetadata <- function(series, data = getDatasets()) {
   metadata <- data[which(data$DatasetCode == series),3]
   tempxml <- tempfile("temp", fileext = ".xml")
