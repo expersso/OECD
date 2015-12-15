2e47ea385d3692611cea2cc8a7c9d75b4dfcc96f
Expersso <expersso5@gmail.com>
2015-12-15 10:43:38 +0100

    Add tests.

---
+++ tests/testthat/tests.R
@@ -0,0 +1,16 @@
+dataset <- "DUR_D"
+
+test_that("get_data_structure returns a data frame with positive length", {
+  dstruc <- get_data_structure(dataset)
+
+  expect_equal(class(dstruc), "list")
+  expect_true(length(dstruc) > 0)
+})
+
+test_that("get_dataset returns a data frame with positive length", {
+  filter_list <- list(c("DEU", "FRA"), "MW", "2024")
+  df <- get_dataset(dataset = dataset, filter = filter_list)
+  
+  expect_equal(class(df), c("tbl_df", "tbl", "data.frame"))
+  expect_true(nrow(df) > 0)
+})
